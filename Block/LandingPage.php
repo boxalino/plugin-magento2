40097c5371e61b3739f9d24ff3e7e34c2d354c32
LLAETSCH\Matthew <matthew.johnson@boxalino.com>
2017-11-09 16:29:26 +0100

    added constructor to the Landingpage Block for the check 'isActive'

--- Block/LandingPage.php
+++ Block/LandingPage.php
@@ -8,13 +8,25 @@ namespace Boxalino\Intelligence\Block;
  */
 class LandingPage extends \Magento\Framework\View\Element\Template{
 
+  private $bxHelperData;
+  private $p13nHelper;
+
+  public function __construct(
+      \Magento\Framework\View\Element\Template\Context $context,
+      \Boxalino\Intelligence\Helper\P13n\Adapter $p13nHelper,
+      \Boxalino\Intelligence\Helper\Data $bxHelperData,
+      array $data = []
+  ){
+
+      parent::__construct($context, $data);
+      $this->p13nHelper = $p13nHelper;
+      $this->bxHelperData = $bxHelperData;
+
+  }
+
   public function isActive(){
 
-      if ($this->bxHelperData->isPluginEnabled()) {
-
-          return true;
-
-      }
+    return $this->bxHelperData->isPluginEnabled();
 
   }
 
