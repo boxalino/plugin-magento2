96ab5f172ab19e56abf8bdd0c867cf0366f06f29
Dana Negrescu <dana@boxalino.com>
2019-03-21 13:43:23 +0100

    check if defined BP

--- registration.php
+++ registration.php
@@ -5,10 +5,12 @@
     __DIR__
 );
 
-$vendorDir = require BP . '/app/etc/vendor_path.php';
-$vendorAutoload = BP . "/{$vendorDir}/autoload.php";
-/** @var \Composer\Autoload\ClassLoader $composerAutoloader */
-$composerAutoloader = include $vendorAutoload;
-$composerAutoloader->addPsr4('com\\boxalino\\bxclient\\v1\\', array(__DIR__ . '/Lib'));
-$composerAutoloader->addPsr4('com\\boxalino\\p13n\\api\\thrift\\', array(__DIR__ . '/Lib/P13n'));
-$composerAutoloader->addPsr4('Thrift\\', array(__DIR__ . '/Lib/Thrift'));
+if (defined('BP')) {
+    $vendorDir = require BP . '/app/etc/vendor_path.php';
+    $vendorAutoload = BP . "/{$vendorDir}/autoload.php";
+    /** @var \Composer\Autoload\ClassLoader $composerAutoloader */
+    $composerAutoloader = include $vendorAutoload;
+    $composerAutoloader->addPsr4('com\\boxalino\\bxclient\\v1\\', array(__DIR__ . '/Lib'));
+    $composerAutoloader->addPsr4('com\\boxalino\\p13n\\api\\thrift\\', array(__DIR__ . '/Lib/P13n'));
+    $composerAutoloader->addPsr4('Thrift\\', array(__DIR__ . '/Lib/Thrift'));
+}
