16a794a66cdfcf212fc3ad2603f5c192425593f6
Dana Negrescu <dana@boxalino.com>
2019-03-26 18:08:06 +0100

    remove BP definition;

--- registration.php
+++ registration.php
@@ -4,13 +4,12 @@
     'Boxalino_Intelligence',
     __DIR__
 );
-if (!defined('BP')) {
-    define('BP', dirname(\Composer\Factory::getComposerFile()));
+if (defined('BP')) {
+    $vendorDir = require BP . '/app/etc/vendor_path.php';
+    $vendorAutoload = BP . "/{$vendorDir}/autoload.php";
+    /** @var \Composer\Autoload\ClassLoader $composerAutoloader */
+    $composerAutoloader = include $vendorAutoload;
+    $composerAutoloader->addPsr4('com\\boxalino\\bxclient\\v1\\', array(__DIR__ . '/Lib'));
+    $composerAutoloader->addPsr4('com\\boxalino\\p13n\\api\\thrift\\', array(__DIR__ . '/Lib/P13n'));
+    $composerAutoloader->addPsr4('Thrift\\', array(__DIR__ . '/Lib/Thrift'));
 }
-$vendorDir = require BP . '/app/etc/vendor_path.php';
-$vendorAutoload = BP . "/{$vendorDir}/autoload.php";
-/** @var \Composer\Autoload\ClassLoader $composerAutoloader */
-$composerAutoloader = include $vendorAutoload;
-$composerAutoloader->addPsr4('com\\boxalino\\bxclient\\v1\\', array(__DIR__ . '/Lib'));
-$composerAutoloader->addPsr4('com\\boxalino\\p13n\\api\\thrift\\', array(__DIR__ . '/Lib/P13n'));
-$composerAutoloader->addPsr4('Thrift\\', array(__DIR__ . '/Lib/Thrift'));
